e4dd0312acddf5f0daf0cd3888651653fad26798
LFDIAZDEV2209 <201288669+LFDIAZDEV2209@users.noreply.github.com>
2025-08-14 17:57:26 +0000

    feat: :sparkles: add Lineage entity and its configuration for database management

---
+++ Src/Shared/Configuration/LineageConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ColombianCoffee.src.Modules.Varieties.Domain.Entities;
+
+namespace ColombianCoffee.src.Shared.Configuration;
+
+public class LineageConfiguration : IEntityTypeConfiguration<Lineage>
+{
+    public void Configure(EntityTypeBuilder<Lineage> b)
+    {
+        b.ToTable("lineage");
+        b.HasKey(x => x.Id);
+        b.Property(x => x.Id).HasColumnName("id");
+        b.Property(x => x.Name).HasColumnName("name").HasMaxLength(150).IsRequired();
+        b.Property(x => x.Description).HasColumnName("description");
+        b.Property(x => x.Notes).HasColumnName("notes");
+    }
+}
