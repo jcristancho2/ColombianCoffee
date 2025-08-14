c660b72a82751dde47c1e2dca1521e0be66509d4
LFDIAZDEV2209 <201288669+LFDIAZDEV2209@users.noreply.github.com>
2025-08-14 18:10:29 +0000

    feat: :sparkles: create IVarietyService interface for managing coffee variety operations

---
+++ Src/Modules/Varieties/Application/Interfaces/IVarietyService.cs
@@ -0,0 +1,9 @@
+using ColombianCoffee.src.Modules.Varieties.Application.DTOs;
+
+namespace ColombianCoffee.src.Modules.Varieties.Application.Interfaces;
+
+public interface IVarietyService
+{
+    Task<IEnumerable<VarietyIdNameDto>> GetFilteredVarietiesAsync(VarietyFilterDto filter, CancellationToken ct = default);
+    Task<VarietyDetailDto?> GetVarietyDetailAsync(uint id, CancellationToken ct = default);
+}
