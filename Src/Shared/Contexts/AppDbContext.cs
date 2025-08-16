e63d3a3b45789a238c1ab6b2d42761214250b5d5
jcristancho2 <jocristanchool@gmail.com>
2025-08-16 17:21:56 -0500

    refactor: :sparkles: integracion de nuevos componentes al appDbcontext

--- Src/Shared/Contexts/AppDbContext.cs
+++ Src/Shared/Contexts/AppDbContext.cs
@@ -1,5 +1,8 @@
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using ColombianCoffee.Src.Modules.Auth.Domain.Entities;
 using ColombianCoffee.src.Modules.Varieties.Domain.Entities;
+using ColombianCoffee.Modules.Varieties.Domain.Entities;
 
 
 namespace ColombianCoffee.Src.Shared.Contexts
@@ -9,7 +12,7 @@ namespace ColombianCoffee.Src.Shared.Contexts
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
+        public DbSet<User> Users { get; set; }
         public DbSet<Variety> Varieties { get; set; }
         public DbSet<Species> Species { get; set; }
         public DbSet<GeneticGroup> GeneticGroup { get; set; }
@@ -19,7 +22,47 @@ namespace ColombianCoffee.Src.Shared.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Aplicar configuraciones desde el ensamblado
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            // Configuración explícita de User
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("Users");
+
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.Username)
+                    .HasColumnName("Name")
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(u => u.Email)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(u => u.PasswordHash)
+                    .IsRequired()
+                    .HasMaxLength(255);
+
+                entity.Property(u => u.Role)
+                    .IsRequired();
+
+                entity.HasIndex(u => u.Email)
+                    .IsUnique();
+
+                entity.HasIndex(u => u.Username)
+                    .IsUnique();
+            });
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entity.GetTableName();
+                if (!string.IsNullOrEmpty(tableName))
+                {
+                    entity.SetTableName(tableName.ToLower());
+                }
+            }
 
             base.OnModelCreating(modelBuilder);
         }
